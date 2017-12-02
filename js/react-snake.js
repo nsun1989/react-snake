ca606151911e041385f93b2f28dad60e77f1eeef
Nan Sun <nan.sun@hudsonltd.com>
2017-12-02 18:53:18 -0500

    add render function for game control panel and game display table

--- js/react-snake.js
+++ js/react-snake.js
@@ -1,15 +1,86 @@
+var snake = function () {
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(
-    element,
-    document.getElementById('root')
-  );
+	var SnakeTableCell = React.createClass({
+		render: function() {
+      return (
+      	<span className="snake-table-cell">	
+      	</span>
+      );
+    }
+	});
+
+	var SnakeTable = React.createClass({
+	  render: function() {
+	  	let row = this.props.row || 10;
+	  	let col = this.props.col || 10;
+
+      var table = [];
+      var tr = [];
+
+			for (let i=0; i < col; i++) {
+			  tr.push(<td><SnakeTableCell/></td>);
+			}
+
+			for (let i=0; i < row; i++) {
+			  table.push(<tr><td>{tr}</td></tr>);
+			}			
+
+      return (
+      	<table>      	
+        	{table}
+      	</table>
+      );
+    }
+	});
+
+	var ControlPanel = React.createClass({
+	  render: function() {
+      return (
+      	<div>   
+  				<input type="radio" className="" name="gender" value="Low" onClick={this.props.gameControl({"speed":0})}/> Low
+  				<input type="radio" className="" name="gender" value="Mid" onClick={this.props.gameControl({"speed":1})}/> Mid
+  				<input type="radio" className="" name="gender" value="Hig" onClick={this.props.gameControl({"speed":2})}/> Hig
+      		<button type="button" className="" onClick={this.props.gameControl({"speed":0})}>Start Game</button>
+      	</div>
+      );
+    }
+	});
+
+	var SnakeGame = React.createClass({
+		getInitialState: function() {
+      return {
+        palse: true,
+        col:20,
+        row:10,
+        speed: ''
+      }
+    },
+    gameControl:function(obj){
+    	console.error("test");
+    },
+	  render: function() {
+      return (
+			  <div>
+			    <SnakeTable 
+			    	row={this.state.row} 
+			    	col={this.state.col}
+			    	speed={this.state.speed}/>
+
+			    <ControlPanel
+			    	gameControl={this.gameControl}/>
+			  </div>
+      );
+    }
+	});
+
+	var destination = document.querySelector("#root");
+	
+	ReactDOM.render(
+	  <div>
+	    <SnakeGame/>
+	  </div>,
+	  destination
+	);  
 }
 
-setInterval(tick, 1000);
+snake();
